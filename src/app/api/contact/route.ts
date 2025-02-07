e4bff9861c748f91d5852dae3765ce347bbd7b67
Hiralvala <valahiral563@gmail.com>
2025-02-07 19:51:06 +0530

    Added user schema

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,5 @@
+// async function connectToDatabase(){
+//     try{
+//         if(mongoose.connection.readystate)
+//     }
+// }
---
+++ src/models/Contact.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    phoneNumber:{
+        type:String,
+        required:true,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+// Check if the model already exists
+const Contact =  mongoose.model.Contact || mongoose.model('Contact', contactSchema);
+export default Contact;
